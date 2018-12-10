2731c79882d89202951cd52e5ae045328ddec3c4
Paul Ailincai <paulailincai@icloud.com>
2018-12-10 22:59:19 +0400

    Correct path for icon set

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const kebabCase = require('lodash.kebabcase')
 
 module.exports = function(importName) {
   const name = kebabCase(importName).replace(/-/, '/')
-  return `react-icons/lib/${name}`
+  return `react-icons/${name}`
 }
